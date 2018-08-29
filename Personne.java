412de473562b81d1260407b76bfdc5a9c2ced20e
O <catssshead@gmail.com>
2018-08-29 11:43:45 +0200

    Create Personne.java

---
+++ Personne.java
@@ -0,0 +1,43 @@
+package demoArrayList;
+
+public class Personne {
+	
+	String nom;
+	String prenom;
+	int age;
+	String email;
+	String telephone;
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+}
